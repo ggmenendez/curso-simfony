473a40df53ee401ed39e221d48cd4736e42c071f
Guillermo García <g.gmenendez@openmailbox.org>
2017-10-27 13:58:29 +0200

    Final del día 27-10-2017

--- src/AppBundle/Controller/GradosController.php
+++ src/AppBundle/Controller/GradosController.php
@@ -2,8 +2,14 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Grado;
+use AppBundle\Form\GradoType;
+use AppBundle\Repository\GradoRepository;
+use Doctrine\DBAL\Exception\ForeignKeyConstraintViolationException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpFoundation\Request;
 
 class GradosController extends Controller
 {
@@ -12,39 +18,87 @@ class GradosController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('AppBundle:Grados:index.html.twig', array(
-            // ...
+        $em = $this->getDoctrine()->getManager();
+        $gr = $em->getRepository(Grado::class);
+        $grades = $gr->findAll();
+        return $this->render('Grados/index.html.twig', array(
+            'grades' => $grades
         ));
     }
 
     /**
      * @Route("/grados/new")
      */
-    public function newAction()
+    public function newAction(Request $request)
     {
-        return $this->render('AppBundle:Grados:new.html.twig', array(
-            // ...
+        $grade = new Grado();
+        $form = $this->createForm(GradoType::class, $grade);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $newGrado = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($newGrado);
+            $em->flush();
+
+            return $this->render('Grados/new.html.twig', array(
+                'form' => $form->createView(),
+                'data' => $newGrado
+            ));
+        }
+
+        return $this->render('Grados/new.html.twig', array(
+            'form' => $form->createView(),
+            'data' => null
         ));
     }
 
     /**
-     * @Route("/grados/edit")
+     * @Route("/grados/edit/{id}")
      */
-    public function editAction()
+    public function editAction(Request $request, $id)
     {
-        return $this->render('AppBundle:Grados:edit.html.twig', array(
-            // ...
+        if (!$id) return $this->redirectToRoute('ejercicios_form_grados_new');
+        $em = $this->getDoctrine()->getManager();
+        $gr = $em->getRepository(Grado::class);
+        $grade = $gr->find($id);
+        $form = $this->createForm(GradoType::class, $grade);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $grade = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($grade);
+            $em->flush();
+
+            return $this->redirectToRoute('ejercicios_form_grados_index');
+        }
+
+        return $this->render('Grados/edit.html.twig', array(
+            "form" => $form->createView()
         ));
     }
 
     /**
      * @Route("/delete/{id}")
      */
-    public function deleteAction($id)
+    public function deleteAction ($id)
     {
-        return $this->render('AppBundle:Grados:delete.html.twig', array(
-            // ...
-        ));
+        if (!$id) return $this->redirectToRoute('ejercicios_form_grados_index');
+        $em = $this->getDoctrine()->getManager();
+        $gr = $em->getRepository(Grado::class)->find($id);
+        $em->remove($gr);
+        try{
+            $em->flush();
+        }catch (ForeignKeyConstraintViolationException $e) {
+            return $this->render('Grados/delete.html.twig', array(
+                "error" => $e->getMessage()
+            ));
+        }
+
+        return $this->redirectToRoute('ejercicios_form_grados_index');
     }
 
 }
